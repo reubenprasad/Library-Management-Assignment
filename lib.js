de9d80e702ace92fb78dd387b224c7a49636e0e0
reubenprasad <reubenprasad@gmail.com>
2019-07-31 06:43:57 +0530

    books

---
+++ lib.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("#t1").click(function(){
+        
+        $.ajax({
+            type:"GET",
+            url:"books.json",
+            success:function(data){
+            
+               var output ="<table><thead><tr><th>ID</th><th>Title</th><th>Author</th><th>Cover</th><th>Category</th><th>Price</th></tr></thead>";
+             
+               for(var i in data){
+                 
+                     output+="<tr><td>"+data[i].id+"</td>"
+                     output+="<td>"+data[i].title+"</td>";
+                     output+="<td>"+data[i].author+"</td>";
+                     output+="<td><img src='"+data[i].image+"'></td>";
+                     output+="<td>"+data[i].category+"</td>";
+                     output+="<td>"+data[i].price+"</td></tr>";                                    
+               }
+               output+= "</table>";
+               $("#result").html(output);
+            }
+        });
+    });
+});
